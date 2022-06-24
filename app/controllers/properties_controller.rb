9e7ad8a5f68e69ab030e1b37406aa05e475b4531
kento kadowaki <kadowakikento@gmail.com>
2022-06-25 02:48:18 +0900

    物件登録の機能を追加

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,43 @@
 class PropertiesController < ApplicationController
+  
+  def index 
+    @properties = Property.all.order("id DESC")
+  end
+
+  def new 
+    @property = Property.new
+    render :index and return if params[:back]
+  end
+
+  def create 
+    @property = Property.build(property_params)
+    if @property.save
+      redirect_to properties_path, notice: "物件情報を登録しました！"
+    else
+      render :new
+    end
+  end
+
+  def show 
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy 
+
+  end
+
+  private 
+
+  def property_params
+    params.require(:property).permit(%i[name price address age remarks])
+  end
+
 end
